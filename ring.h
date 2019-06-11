34c8e13500103efb702c0de2270fd5943fe5dc76
dtpandey48 <dtpandey48@gmail.com>
2019-06-10 22:04:57 -0700

    Update ring.h

--- ring.h
+++ ring.h
@@ -5,10 +5,37 @@ using namespace std;
 //template<typename T> works same as class
 template <class T>
 class ring {
- 
+  private:  
+      int m_pos;
+      int m_size;
+      T *m_values;
   public:
       class iterator;
+   
+  public
+      ring(int size): m_pos(0); m_size(size), m_values(NULL) {// constructor with data initialization, we can replace NULL with 0
+                                                    // the order of variales matter
+          m_values = new T[size]; 
+      }
+ 
+      ~ring() {
+          delete[] m_values;
+      }
+ 
+      int size() {
+         return m_size;
+      }
+ 
+      void add(T value) {
+         m_values[m_pos] = value;
+          
+         ++m_pos;
+         m_pos = m_pos % m_size;
+      }
       
+       T &get(int i) {
+          return m_values[i];
+       }
 };
 
 template<class T>
