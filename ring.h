831abff1e8aa79a87b597cfb84aba73559eb8ee6
dtpandey48 <dtpandey48@gmail.com>
2019-06-10 21:48:40 -0700

    Update ring.h

--- ring.h
+++ ring.h
@@ -1,1 +1,24 @@
 
+#include <iostream>
+using namespace std;
+
+//template<typename T> works same as class
+template <class T>
+class ring {
+ 
+  public:
+      class iterator;
+      
+};
+
+template<class T>
+class ring<T>::iterator { // we want the ring to use the template of ring template class, hence we pass the T
+        public:
+            void print() {
+             << "Hello from iterator" << T() << endl; // T() will put default value since it is a constructor
+            }
+      };
+
+/*void ring::iterator::print() {
+             << "Hello iterator" << endl; 
+}*/
