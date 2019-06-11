4620981db4f449c8639b9c60c5c25d3f3b814889
dtpandey48 <dtpandey48@gmail.com>
2019-06-10 21:50:26 -0700

    Update nestedTemplateClass.cpp

--- nestedTemplateClass.cpp
+++ nestedTemplateClass.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include "ring.h"
 using namespace std;
 
 
-
 int main() {
-
+  ring<string>::iterator it;
+  it.print();
+  
+  ring<int>::iterator itt;
+  itt.print();
+  
+  /*ring<string> textring(3);
+  
+  textring.add("one");
+  textring.add("two");
+  textring.add("three");
+  textring.add("four");
+  
+  for (int i=0; i < textring.size(); i++) {
+       cout << textring.get(i) << endl; 
+  }*/
 
 
 
