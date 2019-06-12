d4bf44e0b50ba9bf51f7b85402453076970e2e8d
dtpandey48 <dtpandey48@gmail.com>
2019-06-11 22:38:32 -0700

    Update lambda_parameters_return_types_50.cpp

--- lambda_parameters_return_types_50.cpp
+++ lambda_parameters_return_types_50.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void test(void (*pFunc)() ) {
-  pFunc(); 
+void testGreet(void (*greet)(string) ) {
+    greet("Adam");
+}
+
+void runDivide(double (*divide)(double a, double b)) {
+    cout << divide(20.0,5.0) << endl; 
 }
 
 int main() {
-    auto func = [](){cout << "Hello"<<endl;}; // type is like a functor
-    func();
-    [](){cout << "World"<<endl;}();
+    auto pGreet = [](string name){cout << "Hello" << name <<endl;}; // type is like a functor
   
-    test();
+    pGreet("Mike");
   
-    test([](){cout << "World"<<endl;});
+    testGreet(pGreet);
   
+    auto pDivide = [](double a, double b) -> double { // -> tells what return type do we want otherwise 0 being an int will give error
+        if (b == 0.0) {
+          return 0;
+        }
+        return a/b;
+    };
+  
+    cout << pDivide(10.0,5.0) << endl;
+    cout << pDivide(10.0,0) << endl;
+   
+    runDivide(pDivide);
     return 0;
 }
